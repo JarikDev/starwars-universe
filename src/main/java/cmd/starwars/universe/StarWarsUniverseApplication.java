17818655faef99007c99ba07fc673a78bc66bff7
Your Name <you@example.com>
2025-06-18 14:00:57 +0300

    init commit

---
+++ src/main/java/cmd/starwars/universe/StarWarsUniverseApplication.java
@@ -0,0 +1,13 @@
+package cmd.starwars.universe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsUniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsUniverseApplication.class, args);
+	}
+
+}
